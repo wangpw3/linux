9564eecf253c1bca071e11dec96634b84ea024c1
wangpw3 <2602341690@qq.com>
2024-04-23 14:18:11 +0800

    修改：     Thread/pthread 	修改：     Thread/pthread.c

--- Thread/pthread.c
+++ Thread/pthread.c
@@ -11,12 +11,21 @@ int main(int argc, char const *argv[]) {
 	pthread_create(&id, NULL, fun, (void *)&a);
 	printf("我是主线程, 副线程 id = %ld\n", id);
 	//等到指定线程退出后解除阻塞
-	pthread_join(id, NULL);
-	
+	//pthread_join(id, NULL);
+	sleep(5);
+	//取消线程
+	pthread_cancel(id);
+	//结束副线程
+	pthread_exit(NULL);
 	return 0;
 }
 
 void * fun(void * a) {
-	printf("我是副线程, a = %d\n", * (int *)a);
+	int id = pthread_self();
+	while (1) {
+		sleep(1);
+		printf("我是副线程, a = %d, id = %d\n", * (int *)a, id);
+	}
+	
 	return 0;
 }
